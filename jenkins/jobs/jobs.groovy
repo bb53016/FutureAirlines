bd221711df72ab545015ffaec1fceee32f400ceb
bb53016 <banambab@gmail.com>
2019-12-23 01:55:14 -0500

    Update jobs.groovy

--- jenkins/jobs/jobs.groovy
+++ jenkins/jobs/jobs.groovy
@@ -1,1 +1,25 @@
 
+def gitCreds           = '<GitHub Credentail encoded into Jenkins>'
+def gitBuildRepo       = '<URL to GitHub Repository>' 
+
+pipelineJob("Deployment-Pipeline") {
+  description('')
+  [$class: 'BuildBlockerProperty',
+     blockLevel: 'GLOBAL',
+     blockingJobs: 'Deployment-Pipeline',
+     scanQueueFor: 'buildable',
+     useBuildBlocker: true
+  ]
+  parameters {
+    choiceParam('gitCreds', [gitCreds], '')
+    choiceParam('gitUrl', [gitBuildRepo], '')
+    choiceParam('jenkinsNode', ['master'], 'Node to build on')  
+  }
+  definition {
+    cps {
+      script(readFileFromWorkspace('pipelines/pipelines.groovy'))
+      sandbox()
+    }
+  }
+  logRotator(30,100)
+}
