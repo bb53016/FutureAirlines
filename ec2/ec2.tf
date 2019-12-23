f8e41b4fdbd63362fa0dfba950a3947b9e532447
bb53016 <banambab@gmail.com>
2019-12-23 06:29:35 -0500

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,1 +1,23 @@
 
+data "aws_ami" "webservers_ami" {
+	owners = ["137112412989"]
+	filter {
+		name = "name"
+		values = ["amzn2-ami-hvm-*"]
+	}
+	most_recent = true
+}
+
+resource "aws_instance" "webservers" {
+	count = "${length(var.subnets_cidr)}" 
+	ami = "${data.aws_ami.webservers_ami.id}"
+	instance_type = "${var.instance_type}"
+	security_groups = ["${aws_security_group.webservers.id}"]
+	subnet_id = "${element(aws_subnet.public.*.id,count.index)}"
+	user_data = "${file("install_httpd.sh")}"
+    tags = {
+      Name = "Test-Instance"
+	    Owner = "${var.tagOwner}"
+	    Environment	= "Test-CI"
+  }
+}
