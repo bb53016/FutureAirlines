61b201dfa42534d91df8db65811876d5cb4ae1ac
bb53016 <banambab@gmail.com>
2019-12-23 06:33:24 -0500

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -9,12 +9,12 @@ data "aws_ami" "webservers_ami" {
 }
 
 resource "aws_instance" "webservers" {
-	count = "${length(var.subnets_cidr)}" 
 	ami = "${data.aws_ami.webservers_ami.id}"
 	instance_type = "${var.instance_type}"
 	security_groups = ["${aws_security_group.webservers.id}"]
 	subnet_id = "${element(aws_subnet.public.*.id,count.index)}"
 	user_data = "${file("install_httpd.sh")}"
+	
     tags = {
       Name = "Test-Instance"
 	    Owner = "${var.tagOwner}"
