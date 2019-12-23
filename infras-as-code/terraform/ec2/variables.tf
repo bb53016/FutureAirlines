4f2a384a8f7ec41eef603ea5eff44a30e559c59f
bb53016 <banambab@gmail.com>
2019-12-23 01:50:16 -0500

    Update variables.tf

--- infras-as-code/terraform/ec2/variables.tf
+++ infras-as-code/terraform/ec2/variables.tf
@@ -1,1 +1,18 @@
 
+
+variable "ins_type" {}
+variable "environment" {}
+variable "stack_name" {}
+variable "tag_env" {}
+variable "tag_Owner" {}
+variable "tag_role" {}
+variable "tag_team" {}
+variable "ami" {}
+variable "subnet_id" {}
+variable "mgmt_sg_id" {}
+variable "security_groups_ec2_id" {}
+variable "iam_instance_profile" {}
+variable "ebs_optimized" {}
+variable "volume_size" {}
+variable "availability_zone" {}
+variable "keyname" {}
